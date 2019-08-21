23f23495cb0bd81d71eea1ec479e7712bd053d70
nealrobben <neal.robben@cegeka.com>
2019-08-21 13:02:34 +0200

    Added component style

--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -8,20 +8,3 @@ export interface IProduct{
     starRating:number;
     imageUrl:string;
 }
-
-export class Product implements IProduct{
-       
-    constructor(public productId: number,
-        public productName: string,
-        public productCode: string,
-        public releaseDate: string,
-        public price: number,
-        public description: string,
-        public starRating: number,
-        public imageUrl: string){
-    }
-
-    calculateDiscount(percent:number):number{
-        return this.price - (this.price * percent / 100);
-    }
-}
